a5d0900846cc016cc6401c42c1a21c5f9f7558bc
Gabriel Jauffret <gabjauf@hotmail.fr>
2021-02-09 21:32:51 +0100

    Add security

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,7 @@
 $.getJSON('./generated/articles.json').then((articles) => {
   articles.forEach(article => {
     $('#content').append(`
-      <a href="${article.link}" class="card">
+      <a href="${article.link}" class="card" target="_blank" rel="noopener">
         <img src="${article.image}">
         <h2>${article.title}</h2>
       </a>`
